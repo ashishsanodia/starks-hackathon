0ffae906d9414a563e3236c6d722e899a4536949
Bajrang <asthana.bajrang@gmail.com>
2016-06-11 18:50:16 +0530

    @basthana - added weka classes

---
+++ barclays-hackathon-starks/src/main/java/barclays/hackathon/starks/core/weka/classifiers/ProductClassifier.java
@@ -0,0 +1,15 @@
+package barclays.hackathon.starks.core.weka.classifiers;
+
+import org.springframework.stereotype.Component;
+import weka.core.Instance;
+
+/**
+ * Created by Bajrang on 6/11/2016.
+ */
+@Component
+public class ProductClassifier implements Classifier{
+    @Override
+    public void classify(Instance instanceToClassify) {
+
+    }
+}
