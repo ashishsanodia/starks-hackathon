bf0c2ea26ea385419e6f6be6aac0c42da05416ec
yogesh-garad <yogesh.garad@gmail.com>
2016-06-12 01:00:26 +0530

    Yogesh fixed instance builder

--- barclays-hackathon-starks/src/main/java/barclays/hackathon/starks/core/weka/vo/InstanceBuilder.java
+++ barclays-hackathon-starks/src/main/java/barclays/hackathon/starks/core/weka/vo/InstanceBuilder.java
@@ -14,19 +14,23 @@ public class InstanceBuilder {
 
 		// map different attribute to data schema
 		Instances testInstance = dataSchema.stringFreeStructure();
-		Instance instance = new Instance(5);
+		Instance instance = new Instance(4);
 		
 		Attribute currentCard = testInstance.attribute(0);
 		instance.setValue(currentCard, user.getCard().getIndex());
 
-		Attribute ageGroup = testInstance.attribute(1);
+		
+		Attribute moments = testInstance.attribute(1);
+		instance.setValue(moments, user.getLifemoment().getIndex());
+		
+		Attribute ageGroup = testInstance.attribute(2);
 		instance.setValue(ageGroup, user.getLifemoment().getIndex());
 
-		Attribute location = testInstance.attribute(2);
+		Attribute location = testInstance.attribute(3);
 		instance.setValue(location, user.getLocation().getIndex());
 
-		Attribute offer = testInstance.attribute(3);
-		instance.setValue(offer, individual.);
+//		Attribute offer = testInstance.attribute(4);
+//		instance.setValue(offer, null);
 
 		instance.setDataset(testInstance);
 
@@ -37,19 +41,19 @@ public class InstanceBuilder {
 
 		// map different attribute to data schema
 		Instances testInstance = dataSchema.stringFreeStructure();
-		Instance instance = new Instance(5);
+		Instance instance = new Instance(3);
 		
-		Attribute currentCard = testInstance.attribute(0);
-		instance.setValue(currentCard, user.getCard().getIndex());
-
+		Attribute moments = testInstance.attribute(0);
+		instance.setValue(moments, user.getLifemoment().getIndex());
+		
 		Attribute ageGroup = testInstance.attribute(1);
 		instance.setValue(ageGroup, user.getLifemoment().getIndex());
 
 		Attribute location = testInstance.attribute(2);
 		instance.setValue(location, user.getLocation().getIndex());
 
-		Attribute offer = testInstance.attribute(3);
-		instance.setValue(offer, individual.);
+//		Attribute offer = testInstance.attribute(3);
+//		instance.setValue(offer, null);
 
 		instance.setDataset(testInstance);
 
