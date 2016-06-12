b1c6866f7c39bfed2b8435cdcdee9e35af44a3cc
Bajrang <asthana.bajrang@gmail.com>
2016-06-12 06:56:18 +0530

    @basthana - adding offer page

--- barclays-hackathon-starks/src/main/java/barclays/hackathon/starks/service/MockDataService.java
+++ barclays-hackathon-starks/src/main/java/barclays/hackathon/starks/service/MockDataService.java
@@ -1,14 +1,14 @@
 package barclays.hackathon.starks.service;
 
-import static barclays.hackathon.starks.model.AgeRange.AGE_RANGE_25_35;
-import static barclays.hackathon.starks.model.Card.Arrival;
-import static barclays.hackathon.starks.model.LifeMoment.MARRIAGE;
-import static barclays.hackathon.starks.model.UserLocation.METRO;
-
+import barclays.hackathon.starks.model.User;
 import org.joda.time.LocalDate;
 import org.springframework.stereotype.Component;
 
-import barclays.hackathon.starks.model.User;
+import static barclays.hackathon.starks.model.AgeRange.AGE_RANGE_25_35;
+import static barclays.hackathon.starks.model.Card.Arrival;
+import static barclays.hackathon.starks.model.LifeMoment.BIRTHDAY;
+import static barclays.hackathon.starks.model.LifeMoment.MARRIAGE;
+import static barclays.hackathon.starks.model.UserLocation.METRO;
 
 /**
  * Created by Bajrang on 6/12/2016.
@@ -16,23 +16,31 @@ import barclays.hackathon.starks.model.User;
 @Component
 public class MockDataService {
 
-	public User getMockUser(String email) {
-		if (email.contains("existing_user@mail")) {
-			return getMockDataForExistingUser();
-		}
-		return getMockDataForNewUser(email);
-	}
+    public User getMockUser(String email) {
+        if (email.equalsIgnoreCase("birthday@mail")) {
+            return getMockDataForUserHavingBirthdayLifeMoment(email);
+        }
+        if (email.contains("existing_user@mail")) {
+            return getMockDataForExistingUser();
+        }
+        return getMockDataForNewUser(email);
+    }
 
-	private User getMockDataForExistingUser() {
-		User existingUser = new User("id-1", "Existing User", "existing_user@mail", METRO, new LocalDate(1988, 8, 29),
-				"Travel", MARRIAGE, AGE_RANGE_25_35, Arrival);
-		existingUser.setExistingCustomer(true);
+    private User getMockDataForExistingUser() {
+        User existingUser = new User("id-1", "David", "existing_user@mail", METRO, new LocalDate(1988, 8, 29),
+                "Travel", MARRIAGE, AGE_RANGE_25_35, Arrival);
+        existingUser.setExistingCustomer(true);
 
-		return existingUser;
-	}
+        return existingUser;
+    }
 
-	private User getMockDataForNewUser(String email) {
-		return new User("id-1", "New User", email, METRO, new LocalDate(1988, 8, 29), "Travel", MARRIAGE,
-				AGE_RANGE_25_35, null);
-	}
+    private User getMockDataForNewUser(String email) {
+        return new User("id-1", "David", email, METRO, new LocalDate(1988, 8, 29), "Travel", MARRIAGE,
+                AGE_RANGE_25_35, null);
+    }
+
+    private User getMockDataForUserHavingBirthdayLifeMoment(String email) {
+        return new User("id-1", "David", email, METRO, new LocalDate(1988, 8, 29), "Travel", BIRTHDAY,
+                AGE_RANGE_25_35, Arrival);
+    }
 }
